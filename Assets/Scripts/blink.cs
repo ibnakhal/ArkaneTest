128fd6086081d1bcdd12638211cbde4af1d00dc1
ibnakhal <INAKHAL2@art.edu>
2016-05-03 11:01:30 -0700

    start of Blink script.

---
+++ Assets/Scripts/blink.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class blink : MonoBehaviour {
+    [SerializeField]
+    private GameObject indicator;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetButtonDown("Mouse 1")
+            {
+            Ray teleRay = RaycastHit ( )
+        }
+	}
+}
